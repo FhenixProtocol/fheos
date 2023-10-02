ca007ded488bee053bd6280a3f146ae708ef70d8
Lior Bondarevski <liorbond@gmail.com>
2023-10-02 16:42:31 +0300

    fix

--- go.mod
+++ go.mod
@@ -5,19 +5,41 @@ go 1.19
 require github.com/ethereum/go-ethereum v1.10.26
 
 require (
-	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/bits-and-blooms/bitset v1.5.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.10.0 // indirect
+	github.com/crate-crypto/go-kzg-4844 v0.3.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
+	github.com/ethereum/c-kzg-4844 v0.3.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
+
+replace github.com/fhenixprotocol/go-tfhe => ./go-tfhe
+
+replace github.com/ethereum/go-ethereum => ./go-ethereum
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
-	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
-	github.com/go-ole/go-ole v1.2.1 // indirect
-	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/google/uuid v1.2.0 // indirect
+	github.com/fhenixprotocol/go-tfhe v0.0.0-20230806123311-862edf6c6556
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
-	github.com/tklauser/go-sysconf v0.3.5 // indirect
-	github.com/tklauser/numcpus v0.2.2 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+
 )
--- precompiles/fheOps.go
+++ precompiles/fheOps.go
@@ -1,16 +1,86 @@
 package precompiles
 
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/core/vm"
+	tfhe "github.com/fhenixprotocol/go-tfhe"
+)
+
+var interpreter *vm.EVMInterpreter
+
+func SetEvmInterpreter(i *vm.EVMInterpreter) {
+	interpreter = i
+}
+
 func Lior(a uint32, b uint32) (uint32, error) {
 	return a * b, nil
 }
 
 func Moshe(intput []byte, inputLen uint32) ([1][32]byte, error) {
-	byteSlice := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
+	// byteSlice := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}
 
-	// Convert []byte to [1][32]byte
-	var byteArray [1][32]byte
+	// // Convert []byte to [1][32]byte
+	// var byteArray [1][32]byte
 
-	copy(byteArray[0][:], byteSlice)
+	// copy(byteArray[0][:], byteSlice)
 
-	return byteArray, nil
+	// return byteArray, nil
+	println("TOMMM load keys in (e *fheAdd) Run()")
+	tfheConfig := tfhe.Config{
+		IsOracle:             true,
+		OracleType:           "local",
+		OracleDbPath:         "data/oracle.db",
+		OracleAddress:        "http://127.0.0.1:9001",
+		ServerKeyPath:        "keys/tfhe/sks",
+		ClientKeyPath:        "keys/tfhe/cks",
+		PublicKeyPath:        "keys/tfhe/pks",
+		OraclePrivateKeyPath: "keys/oracle/private-oracle.key",
+		OraclePublicKeyPath:  "keys/oracle/public-oracle.key",
+	}
+
+	err := tfhe.InitTfhe(&tfheConfig)
+	if err != nil {
+		fmt.Printf("TOMMM inside error: %s", err)
+		return [1][32]byte{}, err
+	}
+
+	logger := interpreter.GetEVM().Logger
+	lhs, rhs, err := get2VerifiedOperands(accessibleState, input)
+	if err != nil {
+		logger.Error("fheAdd inputs not verified", "err", err, "input", hex.EncodeToString(input))
+		return nil, err
+	}
+
+	if lhs.ciphertext.UintType != rhs.ciphertext.UintType {
+		msg := "fheAdd operand type mismatch"
+		logger.Error(msg, "lhs", lhs.ciphertext.UintType, "rhs", rhs.ciphertext.UintType)
+		return nil, errors.New(msg)
+	}
+
+	// If we are doing gas estimation, skip execution and insert a random ciphertext as a result.
+	// if !accessibleState.Interpreter().evm.Commit && !accessibleState.Interpreter().evm.EthCall {
+	// 	return importRandomCiphertext(accessibleState, lhs.ciphertext.UintType), nil
+	// }
+
+	result, err := lhs.ciphertext.Add(rhs.ciphertext)
+	if err != nil {
+		logger.Error("fheAdd failed", "err", err)
+		return nil, err
+	}
+	importCiphertext(accessibleState, result)
+
+	// TODO: for testing
+	err = os.WriteFile("/tmp/add_result", result.Serialization, 0644)
+	if err != nil {
+		logger.Error("fheAdd failed to write /tmp/add_result", "err", err)
+		return nil, err
+	}
+
+	resultHash := result.Hash()
+	logger.Info("fheAdd success", "lhs", lhs.ciphertext.Hash().Hex(), "rhs", rhs.ciphertext.Hash().Hex(), "result", resultHash.Hex())
+	return resultHash[:], nil
 }
