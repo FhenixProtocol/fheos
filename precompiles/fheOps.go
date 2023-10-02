de64735e0334fe678dd8225db8634076fede5973
Lior Bondarevski <liorbond@gmail.com>
2023-10-02 18:15:22 +0300

    First function

--- precompiles/fheOps.go
+++ precompiles/fheOps.go
@@ -16,20 +16,58 @@ func SetEvmInterpreter(i *vm.EVMInterpreter) {
 	interpreter = i
 }
 
-func get2VerifiedOperands(input []byte) (lhs *verifiedCiphertext, rhs *verifiedCiphertext, err error) {
+func isVerifiedAtCurrentDepth(ct *vm.VerifiedCiphertext) bool {
+	return ct.GetVerifiedDepths().Has(interpreter.GetEVM().GetDepth())
+}
+
+func getVerifiedCiphertextFromEVM(ciphertextHash tfhe.Hash) *vm.VerifiedCiphertext {
+	ct, ok := interpreter.GetVerifiedCiphertexts()[ciphertextHash]
+	if ok && isVerifiedAtCurrentDepth(ct) {
+		return ct
+	}
+	return nil
+}
+
+func getVerifiedCiphertext(ciphertextHash tfhe.Hash) *vm.VerifiedCiphertext {
+	return getVerifiedCiphertextFromEVM(ciphertextHash)
+}
+
+func get2VerifiedOperands(input []byte) (lhs *vm.VerifiedCiphertext, rhs *vm.VerifiedCiphertext, err error) {
 	if len(input) != 65 {
 		return nil, nil, errors.New("input needs to contain two 256-bit sized values and 1 8-bit value")
 	}
-	lhs = getVerifiedCiphertext(accessibleState, tfhe.BytesToHash(input[0:32]))
+	lhs = getVerifiedCiphertext(tfhe.BytesToHash(input[0:32]))
 	if lhs == nil {
 		return nil, nil, errors.New("unverified ciphertext handle")
 	}
-	rhs = getVerifiedCiphertext(accessibleState, tfhe.BytesToHash(input[32:64]))
+	rhs = getVerifiedCiphertext(tfhe.BytesToHash(input[32:64]))
 	if rhs == nil {
 		return nil, nil, errors.New("unverified ciphertext handle")
 	}
 	err = nil
 	return
+}
+
+func importCiphertextToEVMAtDepth(ct *tfhe.Ciphertext, depth int) *vm.VerifiedCiphertext {
+	existing, ok := interpreter.GetVerifiedCiphertexts()[ct.Hash()]
+	if ok {
+		existing.GetVerifiedDepths().Add(depth)
+		return existing
+	} else {
+		verifiedDepths := vm.NewDepthSet()
+		verifiedDepths.Add(depth)
+		new := vm.NewVerifiedCiphertext(verifiedDepths, ct)
+		interpreter.GetVerifiedCiphertexts()[ct.Hash()] = new
+		return new
+	}
+}
+
+func importCiphertextToEVM(ct *tfhe.Ciphertext) *vm.VerifiedCiphertext {
+	return importCiphertextToEVMAtDepth(ct, interpreter.GetEVM().GetDepth())
+}
+
+func importCiphertext(ct *tfhe.Ciphertext) *vm.VerifiedCiphertext {
+	return importCiphertextToEVM(ct)
 }
 
 func Lior(a uint32, b uint32) (uint32, error) {
@@ -78,9 +116,9 @@ func Moshe(input []byte, inputLen uint32) ([1][32]byte, error) {
 		return [1][32]byte{}, err
 	}
 
-	if lhs.ciphertext.UintType != rhs.ciphertext.UintType {
+	if lhs.GetCiphertext().UintType != rhs.GetCiphertext().UintType {
 		msg := "fheAdd operand type mismatch"
-		logger.Error(msg, "lhs", lhs.ciphertext.UintType, "rhs", rhs.ciphertext.UintType)
+		logger.Error(msg, "lhs", lhs.GetCiphertext().UintType, "rhs", rhs.GetCiphertext().UintType)
 		return [1][32]byte{}, errors.New(msg)
 	}
 
@@ -89,12 +127,12 @@ func Moshe(input []byte, inputLen uint32) ([1][32]byte, error) {
 	// 	return importRandomCiphertext(accessibleState, lhs.ciphertext.UintType), nil
 	// }
 
-	result, err := lhs.ciphertext.Add(rhs.ciphertext)
+	result, err := lhs.GetCiphertext().Add(rhs.GetCiphertext())
 	if err != nil {
 		logger.Error("fheAdd failed", "err", err)
 		return [1][32]byte{}, err
 	}
-	importCiphertext(accessibleState, result)
+	importCiphertext(result)
 
 	// TODO: for testing
 	err = os.WriteFile("/tmp/add_result", result.Serialization, 0644)
@@ -104,6 +142,9 @@ func Moshe(input []byte, inputLen uint32) ([1][32]byte, error) {
 	}
 
 	resultHash := result.Hash()
-	logger.Info("fheAdd success", "lhs", lhs.ciphertext.Hash().Hex(), "rhs", rhs.ciphertext.Hash().Hex(), "result", resultHash.Hex())
-	return resultHash[:], nil
+	logger.Info("fheAdd success", "lhs", lhs.GetCiphertext().Hash().Hex(), "rhs", rhs.GetCiphertext().Hash().Hex(), "result", resultHash.Hex())
+	var byteArray [1][32]byte
+
+	copy(byteArray[0][:], resultHash[:])
+	return byteArray, nil
 }
