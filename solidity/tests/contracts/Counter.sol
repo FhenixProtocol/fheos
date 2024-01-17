33dfe48c45eb543e0b79a68e32aa5c1e73c5bb4a
toml01 <7473870+toml01@users.noreply.github.com>
2024-01-17 19:26:02 +0200

    Add tests for uninitialized variables

---
+++ solidity/tests/contracts/Counter.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity >=0.8.19 <0.9.0;
+
+import '../../FHE.sol';
+
+contract Counter {
+  euint8 private counter;
+  mapping(uint256 => euint8) counterMapping;
+
+  ebool private boolean = FHE.asEbool(true);
+
+  function add(bytes calldata encryptedValue) public {
+    euint8 value = FHE.asEuint8(encryptedValue);
+    counter = FHE.add(counter, value);
+  }
+
+  function addMapping(uint8 idx, bytes calldata encryptedValue) public {
+    euint8 value = FHE.asEuint8(encryptedValue);
+    counterMapping[idx] = FHE.add(counterMapping[idx], value);
+  }
+
+  function getCounter(bytes32 publicKey) public view returns (bytes memory) {
+    return FHE.sealoutput(counter, publicKey);
+  }
+
+  function getCounterMapping(
+    uint8 idx,
+    bytes32 publicKey
+  ) public view returns (bytes memory) {
+    return FHE.sealoutput(counterMapping[idx], publicKey);
+  }
+}
