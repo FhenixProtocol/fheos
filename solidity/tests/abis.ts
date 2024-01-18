68be5008b58e41e0d8ab826d6548c65923b09a55
Tom Langer <7473870+toml01@users.noreply.github.com>
2024-01-18 11:15:51 +0200

    Treat uninitialized variables as 0 (#48)
    * Treat uninitialized as 0 (integers and ebool) & Prettier

--- solidity/tests/abis.ts
+++ solidity/tests/abis.ts
@@ -1,92 +1,92 @@
-import { Contract } from 'ethers';
-export interface AddTestType extends Contract {
+import { BaseContract } from 'ethers';
+export interface AddTestType extends BaseContract {
     add: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface SealoutputTestType extends Contract {
+export interface SealoutputTestType extends BaseContract {
     sealoutput: (test: string, a: bigint, pubkey: Uint8Array) => Promise<Uint8Array>;
 }
-export interface LteTestType extends Contract {
+export interface LteTestType extends BaseContract {
     lte: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface SubTestType extends Contract {
+export interface SubTestType extends BaseContract {
     sub: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface MulTestType extends Contract {
+export interface MulTestType extends BaseContract {
     mul: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface LtTestType extends Contract {
+export interface LtTestType extends BaseContract {
     lt: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface SelectTestType extends Contract {
+export interface SelectTestType extends BaseContract {
     select: (test: string, c: boolean, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface ReqTestType extends Contract {
-    req: (test: string, a: bigint) => Promise<()>;
+export interface ReqTestType extends BaseContract {
+    req: (test: string, a: bigint) => Promise<{}>;
 }
-export interface DivTestType extends Contract {
+export interface DivTestType extends BaseContract {
     div: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface GtTestType extends Contract {
+export interface GtTestType extends BaseContract {
     gt: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface GteTestType extends Contract {
+export interface GteTestType extends BaseContract {
     gte: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface RemTestType extends Contract {
+export interface RemTestType extends BaseContract {
     rem: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface AndTestType extends Contract {
+export interface AndTestType extends BaseContract {
     and: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface OrTestType extends Contract {
+export interface OrTestType extends BaseContract {
     or: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface XorTestType extends Contract {
+export interface XorTestType extends BaseContract {
     xor: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface EqTestType extends Contract {
+export interface EqTestType extends BaseContract {
     eq: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface NeTestType extends Contract {
+export interface NeTestType extends BaseContract {
     ne: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface MinTestType extends Contract {
+export interface MinTestType extends BaseContract {
     min: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface MaxTestType extends Contract {
+export interface MaxTestType extends BaseContract {
     max: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface ShlTestType extends Contract {
+export interface ShlTestType extends BaseContract {
     shl: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface ShrTestType extends Contract {
+export interface ShrTestType extends BaseContract {
     shr: (test: string, a: bigint, b: bigint) => Promise<bigint>;
 }
-export interface NotTestType extends Contract {
+export interface NotTestType extends BaseContract {
     not: (test: string, a: bigint) => Promise<bigint>;
 }
-export interface AsEboolTestType extends Contract {
+export interface AsEboolTestType extends BaseContract {
     castFromEuint8ToEbool: (val: bigint, test: string) => Promise<boolean>;
     castFromEuint16ToEbool: (val: bigint, test: string) => Promise<boolean>;
     castFromEuint32ToEbool: (val: bigint, test: string) => Promise<boolean>;
     castFromPlaintextToEbool: (val: bigint) => Promise<boolean>;
     castFromPreEncryptedToEbool: (val: Uint8Array) => Promise<boolean>;
 }
-export interface AsEuint8TestType extends Contract {
+export interface AsEuint8TestType extends BaseContract {
     castFromEboolToEuint8: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint16ToEuint8: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint32ToEuint8: (val: bigint, test: string) => Promise<bigint>;
     castFromPlaintextToEuint8: (val: bigint) => Promise<bigint>;
     castFromPreEncryptedToEuint8: (val: Uint8Array) => Promise<bigint>;
 }
-export interface AsEuint16TestType extends Contract {
+export interface AsEuint16TestType extends BaseContract {
     castFromEboolToEuint16: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint8ToEuint16: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint32ToEuint16: (val: bigint, test: string) => Promise<bigint>;
     castFromPlaintextToEuint16: (val: bigint) => Promise<bigint>;
     castFromPreEncryptedToEuint16: (val: Uint8Array) => Promise<bigint>;
 }
-export interface AsEuint32TestType extends Contract {
+export interface AsEuint32TestType extends BaseContract {
     castFromEboolToEuint32: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint8ToEuint32: (val: bigint, test: string) => Promise<bigint>;
     castFromEuint16ToEuint32: (val: bigint, test: string) => Promise<bigint>;
---
+++ solidity/tests/uninit.test.ts
@@ -0,0 +1,70 @@
+import { Counter } from '../types/tests/contracts/Counter';
+import { createFheInstance, deployContract } from './utils';
+
+describe('Test Unitialized Variables', () => {
+  let contract: Counter;
+  let contractAddr: string;
+
+  it('Contract Deployment', async () => {
+    contract = (await deployContract('Counter')) as Counter;
+    expect(contract).toBeTruthy();
+    contractAddr = await contract.getAddress();
+    expect(contractAddr).toBeTruthy();
+  });
+
+  it('Test reading uninitialized state variable', async () => {
+    const { instance, permit } = await createFheInstance(contractAddr);
+    const encCounter = await contract.getCounter(permit.publicKey);
+    const counter = await instance.unseal(contractAddr, encCounter);
+    expect(Number(counter)).toEqual(0);
+  });
+
+  it('Test using uninitialized state variable', async () => {
+    const { instance, permit } = await createFheInstance(contractAddr);
+
+    const encAmount = instance.encrypt_uint8(33);
+    await contract.add(encAmount);
+
+    const encCounter = await contract.getCounter(permit.publicKey);
+    const counter = await instance.unseal(contractAddr, encCounter);
+    expect(Number(counter)).toEqual(33); // Uninitialized state variable should be considered as 0
+  });
+
+  it('Test reading uninitialized mapping state variable', async () => {
+    const { instance, permit } = await createFheInstance(contractAddr);
+
+    let encCounter = await contract.getCounterMapping(
+      BigInt(0),
+      permit.publicKey
+    );
+    let counter = await instance.unseal(contractAddr, encCounter);
+    expect(Number(counter)).toEqual(0);
+
+    // Try a different index
+    encCounter = await contract.getCounterMapping(BigInt(12), permit.publicKey);
+    counter = await instance.unseal(contractAddr, encCounter);
+    expect(Number(counter)).toEqual(0);
+  });
+
+  it('Test using uninitialized mapping state variable', async () => {
+    const { instance, permit } = await createFheInstance(contractAddr);
+
+    const encAmount = instance.encrypt_uint8(34);
+    await contract.addMapping(BigInt(0), encAmount);
+
+    const encCounter = await contract.getCounterMapping(
+      BigInt(0),
+      permit.publicKey
+    );
+    let counter = await instance.unseal(contractAddr, encCounter);
+    expect(Number(counter)).toEqual(34); // Uninitialized state variable should be considered as 0
+
+    // Try a different index
+    const diffCounter = await contract.getCounterMapping(
+      BigInt(12),
+      permit.publicKey
+    );
+    counter = await instance.unseal(contractAddr, diffCounter);
+    expect(Number(counter)).toEqual(0);
+  });
+});
