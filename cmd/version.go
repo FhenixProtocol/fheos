095c94869e14c6697fc25bf26faf326c0ebe6d63
Eshel <eshel@scrtlabs.com>
2024-02-29 20:01:24 +0200

    go-tfhe: fixed version api, printing it on fheos version command

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/fhenixprotocol/go-tfhe"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +16,7 @@ func init() {
 		Short: "Show current fheos version",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("FheOs:", VERSION)
+			fmt.Println("Tfhe-rs:", tfhe.Version())
 			return nil
 		},
 	}
