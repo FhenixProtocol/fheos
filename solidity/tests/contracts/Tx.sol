b093f5e3c77eeb7c0946a988a138d3efeb3293e4
Eshel <eshel@scrtlabs.com>
2024-03-11 16:49:01 +0200

    initial tx test contract

---
+++ solidity/tests/contracts/Tx.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {FHE} from "../../FHE.sol";
+import {Utils} from "./utils/Utils.sol";
+
+error TestNotFound(string test);
+
+contract AddCaller {
+    euint32 private counter;
+    
+    function addTx(inEuint32 value, bytes calldata publicKey) public returns (bytes memory) {
+        counter = counter.add(value.toU32());
+        return counter.seal(publicKey);
+    }
+
+    function addViaContractCallAsPlain(
+        inEuint32 value,
+        address contractAddress,
+        bytes calldata publicKey
+    ) public returns (bytes memory) {
+        AddCallee addContract = AddCallee(contractAddress);
+        counter = addContract.add(FHE.decrypt(counter), FHE.decrypt(value));
+
+        return counter.seal(publicKey);
+    }
+
+    function addViaContractCall(
+        inEuint32 value,
+        address addContract,
+        bytes calldata publicKey
+    ) public returns (bytes memory) {
+        AddCallee addContract = AddCallee(contractAddress);
+        counter = addContract.add(counter, value);
+
+        return counter.seal(publicKey);
+    }
+
+    function addViaContractCallU32(
+        inEuint32 value,
+        address addContract,
+        bytes calldata publicKey
+    ) public returns (bytes memory) {
+        AddCallee addContract = AddCallee(contractAddress);
+        counter = addContract.add(counter, value.toU32());
+
+        return counter.seal(publicKey);
+    }
+
+    function addDelegate(
+        inEuint32 value,
+        address addContract,
+        bytes calldata publicKey
+    ) public returns (bytes memory) {
+        AddCallee addContract = AddCallee(contractAddress);
+        addContract.addDelegate(value);
+
+        return counter.seal(publicKey);
+    }
+
+    function addDelegateU32(
+        inEuint32 value,
+        address addContract,
+        bytes calldata publicKey
+    ) public returns (bytes memory) {
+        AddCallee addContract = AddCallee(contractAddress);
+
+        abi.encodeWithSignature("addDelegate(uint256)", value.toU32());
+
+        return counter.seal(publicKey);
+    }
+}
+
+contract AddCallee {
+    euint32 private counter;
+
+    function add(euint32 a, inEuint32 b) public pure returns (euint32 output) {
+        return a + b.toU32();
+    }
+
+    function add(euint32 a, euint32 b) public pure returns (euint32 output) {
+        return a + b;
+    }
+
+    function add(uint32 a, uint32 b) public pure returns (euint32 output) {
+        return FHE.asEuint32(a + b);
+    }
+
+    function addDelegate(euint32) public {
+        counter = counter + b;
+    }
+
+    function addDelegate(inEuint32) public {
+        counter = counter + b.toU32();
+    }
+}
