fdc0db0ee3e9f68418f902e8913a919264751552
Eshel <eshel@scrtlabs.com>
2024-03-24 17:52:09 +0200

    testing simple tx with and without static call

--- solidity/tests/transaction.test.ts
+++ solidity/tests/transaction.test.ts
@@ -1,10 +1,26 @@
 import { AddCaller, AddCallee } from "../types/tests/contracts/Tx.sol";
 import { createFheInstance, deployContract } from "./utils";
+import {ethers} from "hardhat";
 
 describe.only("Test Transactions Scenarios", () => {
   let contractCaller: AddCaller;
   let contractCallee: AddCallee;
   let contractAddr: string;
+
+  beforeAll(async () => {
+    contractCallee = (await deployContract("AddCallee")) as AddCallee;
+    expect(contractCallee).toBeTruthy();
+
+    contractCaller = (await deployContract("AddCaller", [contractCallee])) as AddCaller;
+    expect(contractCaller).toBeTruthy();
+    contractAddr = await contractCaller.getAddress();
+    expect(contractAddr).toBeTruthy();
+  });
+
+  beforeEach(async () => {
+    const encCounterResponse = await contractCaller.resetCounter();
+    await encCounterResponse.wait();
+  });
 
   it.only("Contract Deployment", async () => {
     contractCallee = (await deployContract("AddCallee")) as AddCallee;
@@ -16,61 +32,22 @@ describe.only("Test Transactions Scenarios", () => {
     expect(contractAddr).toBeTruthy();
   });
 
-  it.only("Test simple Tx - Call Static", async () => {
+  it.only("Test simple Tx - static call ", async () => {
     const { instance, permit } = await createFheInstance(contractAddr);
     const encInput = await instance.encrypt_uint32(7);
     const encCounterResponse = await contractCaller.addTx.staticCall(encInput, permit.publicKey);
     const counter = instance.unseal(contractAddr, encCounterResponse);
-    console.log("counter", counter);
     expect(Number(counter)).toEqual(7);
   });
 
-  // it("Test using uninitialized state variable", async () => {
-  //   const { instance, permit } = await createFheInstance(contractAddr);
-  //
-  //   const encAmount = await instance.encrypt_uint8(33);
-  //   await contract.add(encAmount.data);
-  //
-  //   const encCounter = await contract.getCounter(permit.publicKey);
-  //   const counter = await instance.unseal(contractAddr, encCounter);
-  //   expect(Number(counter)).toEqual(33); // Uninitialized state variable should be considered as 0
-  // });
-  //
-  // it("Test reading uninitialized mapping state variable", async () => {
-  //   const { instance, permit } = await createFheInstance(contractAddr);
-  //
-  //   let encCounter = await contract.getCounterMapping(
-  //     BigInt(0),
-  //     permit.publicKey
-  //   );
-  //   let counter = await instance.unseal(contractAddr, encCounter);
-  //   expect(Number(counter)).toEqual(0);
-  //
-  //   // Try a different index
-  //   encCounter = await contract.getCounterMapping(BigInt(12), permit.publicKey);
-  //   counter = await instance.unseal(contractAddr, encCounter);
-  //   expect(Number(counter)).toEqual(0);
-  // });
-  //
-  // it("Test using uninitialized mapping state variable", async () => {
-  //   const { instance, permit } = await createFheInstance(contractAddr);
-  //
-  //   const encAmount = await instance.encrypt_uint8(34);
-  //   await contract.addMapping(BigInt(0), encAmount.data);
-  //
-  //   const encCounter = await contract.getCounterMapping(
-  //     BigInt(0),
-  //     permit.publicKey
-  //   );
-  //   let counter = await instance.unseal(contractAddr, encCounter);
-  //   expect(Number(counter)).toEqual(34); // Uninitialized state variable should be considered as 0
-  //
-  //   // Try a different index
-  //   const diffCounter = await contract.getCounterMapping(
-  //     BigInt(12),
-  //     permit.publicKey
-  //   );
-  //   counter = await instance.unseal(contractAddr, diffCounter);
-  //   expect(Number(counter)).toEqual(0);
-  // });
+  it.only("Test simple Tx - query for state change", async () => {
+    const { instance, permit } = await createFheInstance(contractAddr);
+    const encInput = await instance.encrypt_uint32(8);
+    const encCounterResponse = await contractCaller.addTx(encInput, permit.publicKey);
+    await encCounterResponse.wait();
+
+    const getCounterResponse = await contractCaller.getCounter(permit.publicKey);
+    const counter = instance.unseal(contractAddr, getCounterResponse);
+    expect(Number(counter)).toEqual(8);
+  });
 });
