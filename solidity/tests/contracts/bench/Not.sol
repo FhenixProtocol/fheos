49403e20e7a374e67f72785207addb75b151d876
Eshel <eshel@scrtlabs.com>
2024-06-07 04:17:02 +0300

    restored NOT as a valid bool operation

--- solidity/tests/contracts/bench/Not.sol
+++ solidity/tests/contracts/bench/Not.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import {FHE} from "../../../FHE.sol";
 import {
+	ebool, inEbool,
 	euint8, inEuint8,
 	euint16, inEuint16,
 	euint32, inEuint32,
@@ -11,12 +12,16 @@ import {
 } from "../../../FHE.sol";
 
 contract NotBench {
+	ebool internal aBool;
 	euint8 internal a8;
 	euint16 internal a16;
 	euint32 internal a32;
 	euint64 internal a64;
 	euint128 internal a128;
 
+    function loadBool(inEbool calldata _a) public {
+        aBool = FHE.asEbool(_a);
+    }
     function load8(inEuint8 calldata _a) public {
         a8 = FHE.asEuint8(_a);
     }
@@ -33,6 +38,9 @@ contract NotBench {
         a128 = FHE.asEuint128(_a);
     }
 
+    function benchNotBool() public view {
+        FHE.not(aBool);
+    }
     function benchNot8() public view {
         FHE.not(a8);
     }
