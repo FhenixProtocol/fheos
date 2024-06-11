b0cddd62fd23c2c3c9c5a4e219d22c8c619d0806
Cashmaney <itzik@fhenix.io>
2024-06-11 12:29:18 +0000

    fix imports

--- solgen/solgen.ts
+++ solgen/solgen.ts
@@ -25,12 +25,15 @@ import {
   testContractReencrypt,
   testContractReq,
   AsTypeTestingContract,
+} from "./templates/testContracts";
+
+import {
   AsTypeBenchmarkContract,
   benchContract1Arg,
   benchContract2Arg,
   benchContract3Arg,
   benchContractReencrypt,
-} from "./templates/testContracts";
+} from "./templates/benchContracts";
 
 import {
   AllTypes,
