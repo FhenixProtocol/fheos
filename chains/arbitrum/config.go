c9c08a30b8b48dde631a073977b1aeadba1e6776
Eshel <eshel@scrtlabs.com>
2024-07-08 15:44:47 +0300

    adding the flag on the command line of nitro to accept fallback address

--- chains/arbitrum/config.go
+++ chains/arbitrum/config.go
@@ -9,6 +9,7 @@ func FhenixConfigAddOptions(prefix string, f *flag.FlagSet) {
 	f.String(prefix+".oracle-type", fhe.ConfigDefault.OracleType, "FHE oracle type")
 	f.String(prefix+".oracle-address", fhe.ConfigDefault.OracleAddress, "FHE oracle address")
 	f.String(prefix+".fhe-engine-address", fhe.ConfigDefault.FheEngineAddress, "FHE engine address")
+	f.String(prefix+".fallback-fhe-engine-address", fhe.ConfigDefault.FallbackFheEngineAddress, "FHE engine fallback address")
 	f.String(prefix+".home-dir", fhe.ConfigDefault.HomeDir, "FHE home directory")
 	f.Int(prefix+".log-level", fhe.ConfigDefault.LogLevel, "Minimum log level to display (0-5)")
 }
