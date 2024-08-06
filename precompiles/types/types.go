79e98ef7c1bd4edcc813c8651b29f93f4e3f61c9
toml01 <7473870+toml01@users.noreply.github.com>
2024-08-06 20:22:06 +0200

    a wip version of `DecryptionResults`

--- precompiles/types/types.go
+++ precompiles/types/types.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"fmt"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/fhenixprotocol/warp-drive/fhe-driver"
 )
@@ -148,4 +151,75 @@ func PrecompileNameFromString(s string) (PrecompileName, bool) {
 type PendingDecryption struct {
 	Hash fhe.Hash
 	Type PrecompileName
+}
+
+type DecryptionResults struct {
+	data map[PendingDecryption]interface{}
+}
+
+func NewDecryptionResultsMap() *DecryptionResults {
+	return &DecryptionResults{
+		data: make(map[PendingDecryption]interface{}),
+	}
+}
+
+func (dr *DecryptionResults) CreateEmptyRecord(key PendingDecryption) {
+	dr.data[key] = nil
+}
+
+func (dr *DecryptionResults) SetValue(key PendingDecryption, value interface{}) error {
+	if _, exists := dr.data[key]; !exists {
+		return fmt.Errorf("record does not exist, create an empty record first")
+	}
+
+	switch key.Type {
+	case SealOutput:
+		if _, ok := value.([]byte); !ok {
+			return fmt.Errorf("value for SealOutput must be []byte")
+		}
+	case Require:
+		if _, ok := value.(bool); !ok {
+			return fmt.Errorf("value for Require must be bool")
+		}
+	case Decrypt:
+		if _, ok := value.(*big.Int); !ok {
+			return fmt.Errorf("value for Decrypt must be *big.Int")
+		}
+	default:
+		return fmt.Errorf("unknown PrecompileName")
+	}
+
+	dr.data[key] = value
+	return nil
+}
+
+func (dr *DecryptionResults) Get(key PendingDecryption) (interface{}, error) {
+	value, exists := dr.data[key]
+	if !exists {
+		return nil, fmt.Errorf("key not found")
+	}
+
+	if value == nil {
+		return nil, nil // Exists but no value
+	}
+
+	switch key.Type {
+	case SealOutput:
+		if bytes, ok := value.([]byte); ok {
+			return bytes, nil
+		}
+		return nil, fmt.Errorf("value is not []byte as expected for SealOutput")
+	case Require:
+		if boolValue, ok := value.(bool); ok {
+			return boolValue, nil
+		}
+		return nil, fmt.Errorf("value is not bool as expected for Require")
+	case Decrypt:
+		if bigInt, ok := value.(*big.Int); ok {
+			return bigInt, nil
+		}
+		return nil, fmt.Errorf("value is not *big.Int as expected for Decrypt")
+	default:
+		return nil, fmt.Errorf("unknown PrecompileName")
+	}
 }
