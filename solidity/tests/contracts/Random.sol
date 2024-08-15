8181ad2636567704aac768e798d15e82a1a2ebd6
Eshel <eshel@scrtlabs.com>
2024-08-15 19:57:12 +0300

    autogen test contract

---
+++ solidity/tests/contracts/Random.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {FHE} from "../../FHE.sol";
+import {Utils} from "./utils/Utils.sol";
+
+error TestNotFound(string test);
+
+contract RandomTest {
+    using Utils for *;
+    
+    function random(string calldata test) public pure returns (uint256 output) {
+        if (Utils.cmp(test, "random_euint8()")) {
+            return FHE.decrypt(FHE.randomEuint8());
+        } else if (Utils.cmp(test, "random_euint16()")) {
+            return FHE.decrypt(FHE.randomEuint16());
+        } else if (Utils.cmp(test, "random_euint32()")) {
+            return FHE.decrypt(FHE.randomEuint32());
+        } else if (Utils.cmp(test, "random_euint64()")) {
+            return FHE.decrypt(FHE.randomEuint64());
+        } else if (Utils.cmp(test, "random_euint128()")) {
+            return FHE.decrypt(FHE.randomEuint128());
+        } else if (Utils.cmp(test, "random_euint256()")) {
+            return FHE.decrypt(FHE.randomEuint256());
+        }}
+}
