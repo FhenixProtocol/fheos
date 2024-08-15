39641973cd8110291dd075e983d0b0d2b0f088dd
Lior Bondarevski <liorbond@gmail.com>
2024-08-15 11:54:54 +0300

    tests

--- solgen/templates/testContracts.ts
+++ solgen/templates/testContracts.ts
@@ -400,51 +400,51 @@ export function testContractDecrypt() {
             euint8 aEnc = FHE.asEuint8(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint16)")) {
+        } else if (Utils.cmp(test, "decrypt(euint16)")) {
             euint16 aEnc = FHE.asEuint16(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint32)")) {
+        } else if (Utils.cmp(test, "decrypt(euint32)")) {
             euint32 aEnc = FHE.asEuint32(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint64)")) {
+        } else if (Utils.cmp(test, "decrypt(euint64)")) {
             euint64 aEnc = FHE.asEuint64(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint128)")) {
+        } else if (Utils.cmp(test, "decrypt(euint128)")) {
             euint128 aEnc = FHE.asEuint128(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint256)")) {
+        } else if (Utils.cmp(test, "decrypt(euint256)")) {
             euint256 aEnc = FHE.asEuint256(1);
             require(aEnc.decrypt(1) < 10, "Decryption failed");
             require(FHE.decrypt(aEnc, 1) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(ebool)")) {
+        } else if (Utils.cmp(test, "decrypt(ebool)")) {
             ebool aEnc = FHE.asEbool(false);
             require(!aEnc.decrypt(false), "Decryption failed");
             require(!FHE.decrypt(aEnc, false), "Decryption failed");
-        } if (Utils.cmp(test, "decrypt(euint8) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint8) fail")) {
             euint8 aEnc = FHE.asEuint8(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint16) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint16) fail")) {
             euint16 aEnc = FHE.asEuint16(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint32) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint32) fail")) {
             euint32 aEnc = FHE.asEuint32(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint64) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint64) fail")) {
             euint64 aEnc = FHE.asEuint64(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint128) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint128) fail")) {
             euint128 aEnc = FHE.asEuint128(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
-        } else if (Utils.cmp(test, "req(euint256) fail")) {
+        } else if (Utils.cmp(test, "decrypt(euint256) fail")) {
             euint256 aEnc = FHE.asEuint256(1);
             require(aEnc.decrypt() < 10, "Decryption failed");
             require(FHE.decrypt(aEnc) < 10, "Decryption failed");
