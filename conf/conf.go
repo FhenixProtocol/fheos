6fe34ea619b179aed0060f6a24972e26e8844b8e
Eshel <eshel@scrtlabs.com>
2024-09-23 18:14:35 +0300

    making fheos-only config

--- conf/conf.go
+++ conf/conf.go
@@ -1,1 +1,17 @@
 package conf
+
+import (
+	flag "github.com/spf13/pflag"
+)
+
+func FheosConfigAddOptions(prefix string, f *flag.FlagSet) {
+	f.String(prefix+".fheos-db-path", ConfigDefault.FheosDbPath, "Path for FheOs database")
+}
+
+type FheosConfig struct {
+	FheosDbPath string `koanf:"fheos-db-path"`
+}
+
+var ConfigDefault = FheosConfig{
+	FheosDbPath: "/tmp/fheos",
+}
