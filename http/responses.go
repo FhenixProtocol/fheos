b59125709150a041f994a8eeae284b45c6162926
liorbond <liorbond@gmail.com>
2025-03-10 14:28:35 +0200

    renaming (#238)

---
+++ http/responses.go
@@ -0,0 +1,31 @@
+package main
+
+type FheOperationResponse struct {
+	TempKey    []byte `json:"tempKey"`
+	ActualHash []byte `json:"actualHash"`
+}
+
+type DecryptResponse struct {
+	CtHash          []byte `json:"ctHash"`
+	Plaintext       string `json:"plaintext"`
+	TransactionHash string `json:"transactionHash"`
+}
+
+type StoreCtsResponse struct {
+	Hashes []string `json:"hashes"`
+}
+type GetNetworkPublicKeyResponse struct {
+	PublicKey string `json:"publicKey"`
+}
+
+type GetCrsResponse struct {
+	Crs string `json:"crs"`
+}
+
+type GetCTResponse struct {
+	Data         string `json:"data"`
+	SecurityZone uint8  `json:"security_zone"`
+	UintType     uint8  `json:"uint_type"`
+	Compact      bool   `json:"compact"`
+	Gzipped      bool   `json:"gzipped"`
+}
